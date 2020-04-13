ddb3d2824f21521819a09537915426c940b31264
restarmilan <restarmilan@gmail.com>
2020-04-13 16:05:04 +0200

    add basic, reusable selenium methods

--- src/main/java/com/rmilan/seleniumtesting/pages/BasePage.java
+++ src/main/java/com/rmilan/seleniumtesting/pages/BasePage.java
@@ -1,6 +1,7 @@
 package com.rmilan.seleniumtesting.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,7 +11,7 @@ public abstract class BasePage {
     final int TIMEOUT = 10;
     WebDriver driver;
     WebDriverWait wait;
-    String baseUrl = "https://www.seleniumeasy.com/test/";
+    String baseUrl = "https://www.seleniumeasy.com/test";
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -21,5 +22,33 @@ public abstract class BasePage {
 
     public void navigateTo() {
         driver.get(baseUrl);
+    }
+
+    public void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    public void clickOnWebElement(WebElement element) {
+        element.click();
+    }
+
+    public void setElementInput(WebElement element, String input) {
+        element.sendKeys(input);
+    }
+
+    public String getPageUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public boolean elementIsDisplayed(WebElement element) {
+        return element.isDisplayed();
+    }
+
+    public String getElementInnerText(WebElement element) {
+        return element.getText();
+    }
+
+    public String getElementAttribute(WebElement element, String attribute) {
+        return element.getAttribute(attribute);
     }
 }
