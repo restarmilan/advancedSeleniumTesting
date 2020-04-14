0fe7b7667da2d2df9b3acfaba904926f2a7ad950
restarmilan <restarmilan@gmail.com>
2020-04-14 12:37:18 +0200

    set driver and log paths as variables in application.properties

--- src/main/java/com/rmilan/seleniumtesting/config/SpringConfig.java
+++ src/main/java/com/rmilan/seleniumtesting/config/SpringConfig.java
@@ -2,20 +2,28 @@ package com.rmilan.seleniumtesting.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("application.properties")
 @ComponentScan(basePackages = "com.rmilan.seleniumtesting.pages")
 public class SpringConfig {
 
+    @Value("${chromeDriverPath}")
+    private String chromeDriverPath;
+
+    @Value("${logsDir}")
+    private String logsDir;
+
     @Bean(name = "webdriver", destroyMethod = "quit")
     public WebDriver getDriver() {
-        String chromeDriverPath = "/home/rmilan/codecool/test_automation/chromedriver";
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        //Create non-verbose chromedriver.log logfile after each run (rewrites previous one); it displays [INFO] level logs
-        System.setProperty("webdriver.chrome.logfile", "/home/rmilan/codecool/advance_java/seleniumtesting/chromedriver.log");
+        //Create non-verbose chrome driver.log logfile after each run (rewrites previous one); it displays [INFO] level logs
+        System.setProperty("webdriver.chrome.logfile", logsDir);
         //uncomment it for verbose logging
         //System.setProperty("webdriver.chrome.verboseLogging", "true");
         WebDriver driver = new ChromeDriver();
