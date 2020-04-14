d42bb2821fc5aacdc918bcc44aa2bea1a599ee50
restarmilan <restarmilan@gmail.com>
2020-04-14 12:02:55 +0200

    create chromedriver logfile after each run

--- src/main/java/com/rmilan/seleniumtesting/config/SpringConfig.java
+++ src/main/java/com/rmilan/seleniumtesting/config/SpringConfig.java
@@ -14,6 +14,10 @@ public class SpringConfig {
     public WebDriver getDriver() {
         String chromeDriverPath = "/home/rmilan/codecool/test_automation/chromedriver";
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        //Create non-verbose chromedriver.log logfile after each run (rewrites previous one); it displays [INFO] level logs
+        System.setProperty("webdriver.chrome.logfile", "/home/rmilan/codecool/advance_java/seleniumtesting/chromedriver.log");
+        //uncomment it for verbose logging
+        //System.setProperty("webdriver.chrome.verboseLogging", "true");
         WebDriver driver = new ChromeDriver();
         return driver;
     }
