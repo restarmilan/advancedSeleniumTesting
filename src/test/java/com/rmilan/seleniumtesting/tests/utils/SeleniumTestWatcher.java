3bb3459183702604b6b29c81f3b3556381aa057a
restarmilan <restarmilan@gmail.com>
2020-04-15 23:50:21 +0200

    TestWatcher works with static screenshot creator method

--- src/test/java/com/rmilan/seleniumtesting/tests/utils/SeleniumTestWatcher.java
+++ src/test/java/com/rmilan/seleniumtesting/tests/utils/SeleniumTestWatcher.java
@@ -4,16 +4,12 @@ import com.rmilan.seleniumtesting.config.SpringConfig;
 import com.rmilan.seleniumtesting.utils.ScreenshotCreator;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.Optional;
 
 @SpringJUnitConfig(SpringConfig.class)
 public class SeleniumTestWatcher implements TestWatcher {
-
-    @Autowired
-    private ScreenshotCreator sc;
 
     @Override
     public void testAborted(ExtensionContext extensionContext, Throwable throwable) {
@@ -28,9 +24,7 @@ public class SeleniumTestWatcher implements TestWatcher {
     @Override
     public void testFailed(ExtensionContext extensionContext, Throwable throwable) {
         System.out.println("Failed test case");
-        System.out.println(sc.toString());
-        sc.createScreenshot();
-        //Displays the message at the failed test case, but throws a NullPointerException. Why??
+        ScreenshotCreator.createScreenshot();
     }
 
     @Override
