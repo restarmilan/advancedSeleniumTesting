bef8aeccca45a1c8dff8b9ad5fe41372f1cfcb80
restarmilan <restarmilan@gmail.com>
2020-04-15 11:43:26 +0200

    add helper class for more detailed test reporting

--- src/test/java/com/rmilan/seleniumtesting/tests/SimpleFormDemoPageTest.java
+++ src/test/java/com/rmilan/seleniumtesting/tests/SimpleFormDemoPageTest.java
@@ -3,6 +3,7 @@ package com.rmilan.seleniumtesting.tests;
 import com.rmilan.seleniumtesting.config.SpringConfig;
 import com.rmilan.seleniumtesting.pages.SimpleFormDemoPage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,26 +12,25 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringJUnitConfig(SpringConfig.class)
+@DisplayNameGeneration(IndicativeSentences.class)
 class SimpleFormDemoPageTest {
 
     @Autowired
     private SimpleFormDemoPage simpleFormDemoPage;
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/simple_input_message.csv", numLinesToSkip = 1)
     @DisplayName("TC-ST-SFD-01 - Check simple input field expected messages")
+    @ParameterizedTest(name="TC-ST-SFD-01 - Check simple input field expected messages with {0} value.")
+    @CsvFileSource(resources = "/simple_input_message.csv", numLinesToSkip = 1)
     void checkSimpleInputField(String input, String expected) {
         simpleFormDemoPage.setSingleInputField(input);
         assertEquals(expected, simpleFormDemoPage.getUserInputFromSingleInputField());
     }
 
-    @ParameterizedTest
+    @DisplayName("TC-ST-SFD-02 - Check double input fields expected added result")
+    @ParameterizedTest(name = "TC-ST-SFD-02 - Check double input fields expected added result with {0} and {0} values.")
     @CsvFileSource(resources = "/double_input_field_test_data.csv", numLinesToSkip = 1)
-    @DisplayName("TC-ST-SFD-02 - Check double input fields expected added values")
     void checkAddedUserInputs(String value1, String value2, String expected) {
         simpleFormDemoPage.setDoubleInputField(value1, value2);
         assertEquals(expected, simpleFormDemoPage.getAddedValuesFromDoubleInputField());
     }
-
-
 }
