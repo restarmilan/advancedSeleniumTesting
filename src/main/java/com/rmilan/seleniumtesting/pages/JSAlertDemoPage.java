a88d30232b7512245f09f0093d3976282096d7f5
restarmilan <restarmilan@gmail.com>
2020-04-25 15:56:01 +0200

    add page objects and methods for prompt alert testing

--- src/main/java/com/rmilan/seleniumtesting/pages/JSAlertDemoPage.java
+++ src/main/java/com/rmilan/seleniumtesting/pages/JSAlertDemoPage.java
@@ -1,5 +1,6 @@
 package com.rmilan.seleniumtesting.pages;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,8 +11,12 @@ public class JSAlertDemoPage extends BasePage {
 
     @FindBy(xpath = "//button[@class='btn btn-default btn-lg']")
     WebElement alertConfirmBoxBtn;
+    @FindBy(xpath = "//button[contains(text(), 'Click for')]")
+    WebElement alertPromptBoxBtn;
     @FindBy(xpath = "//p[@id='confirm-demo']")
     WebElement alertStatus;
+    @FindBy(xpath = "//p[@id='prompt-demo']")
+    WebElement promptAlertMessage;
 
     public JSAlertDemoPage(WebDriver driver) {
         super(driver);
@@ -35,6 +40,28 @@ public class JSAlertDemoPage extends BasePage {
     public String getAlertBoxStatus() {
         return alertStatus.getText();
     }
+
+    public void dismissPromptAlert() {
+        openAlert(alertPromptBoxBtn);
+        driver.switchTo().alert().dismiss();
+    }
+
+    public void setInputToConfirmedPromptAlert(String input) {
+        openAlert(alertPromptBoxBtn);
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys(input);
+        alert.accept();
+    }
+
+    public String getPromptAlertMessage() {
+        return promptAlertMessage.getText();
+    }
+
+    public boolean alertMessageIsNotDisplayed() {
+        return !elementIsDisplayed(promptAlertMessage);
+    }
+
+
 
 
 }
