844d2f1b343315556d8dca538f59a21718189b31
restarmilan <restarmilan@gmail.com>
2020-04-25 15:56:22 +0200

    add test cases for prompt alert confirm / dismiss

--- src/test/java/com/rmilan/seleniumtesting/tests/JsAlertDemoPageTest.java
+++ src/test/java/com/rmilan/seleniumtesting/tests/JsAlertDemoPageTest.java
@@ -8,10 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringJUnitConfig(SpringConfig.class)
 @DisplayNameGeneration(TestNameGenerator.class)
@@ -33,5 +36,24 @@ public class JsAlertDemoPageTest {
     void dismissAlertTest() {
         jsAlertDemoPage.dismissAlert();
         assertEquals(jsAlertDemoPage.getAlertBoxStatus(), "You pressed Cancel!");
+    }
+
+    @Test
+    @DisplayName("TC-ST-JSA-03 - Check prompt alert dismiss")
+    void dismissPromptAlert() {
+        jsAlertDemoPage.dismissPromptAlert();
+        assertTrue(jsAlertDemoPage.alertMessageIsNotDisplayed());
+    }
+
+
+    @DisplayName("TC-ST-JSA-04 - Check accepted prompt alert message")
+    @ParameterizedTest(name = "TC-ST-JSA-04 - Check accepted prompt alert message with {0} value")
+    @CsvFileSource(resources = "/prompt_alert_test_data.csv", numLinesToSkip = 1)
+    void checkMessageOfAcceptedPromptAlert(String input, String expected) {
+        if (input == null) {
+            input = " ";
+        }
+        jsAlertDemoPage.setInputToConfirmedPromptAlert(input);
+        assertEquals(jsAlertDemoPage.getPromptAlertMessage(), expected);
     }
 }
